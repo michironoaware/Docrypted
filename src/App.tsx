edae1926d00faa15891dd93eb4cab0fc771d4167
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-17 19:24:56 -0300

    feat: show file editor in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { FluentProvider, webLightTheme, Theme } from "@fluentui/react-components";
+import FileEditor from "./components/FileEditor.tsx";
+import Header from "./components/Header.tsx";
 
 const customWebLightTheme: Theme = {
 	...webLightTheme,
@@ -7,8 +9,19 @@ const customWebLightTheme: Theme = {
 
 export default function App() {
 	return (
-		<>
-			<FluentProvider theme={customWebLightTheme}></FluentProvider>
-		</>
+		<FluentProvider theme={customWebLightTheme}>
+			<div className={"flex flex-col w-dvw h-dvh"}>
+				<Header />
+				<div
+					className={
+						"px-64 py-16 max-2xl:px-48 max-2xl:py-12 max-xl:px-32 max-xl:py-8" +
+						" " +
+						"transition:padding duration-600 ease-out box-border"
+					}
+				>
+					<FileEditor />
+				</div>
+			</div>
+		</FluentProvider>
 	);
 }
