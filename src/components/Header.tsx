a6c8762897c057be4aa3027f8ee8bc2302b66884
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-19 00:42:39 -0300

    feat: add theme switching

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@fluentui/react-components";
 import { DarkThemeRegular } from "@fluentui/react-icons";
+import { useState } from "react";
 
-export default function Header() {
+export default function Header(props: IHeaderProps) {
 	return (
 		<div
 			className={"flex flex-row justify-between px-16 py-4 max-md:px-8 transition:padding duration-600 ease-out"}
@@ -10,8 +11,12 @@ export default function Header() {
 				<h1 className={"content-center text-lg"}>Docrypted</h1>
 			</div>
 			<div className={"flex flex-row gap-2"}>
-				<Button appearance={"subtle"} icon={<DarkThemeRegular />}></Button>
+				<Button appearance={"subtle"} icon={<DarkThemeRegular />} onClick={props.onThemeButtonClick}></Button>
 			</div>
 		</div>
 	);
+}
+
+export interface IHeaderProps {
+	onThemeButtonClick: () => void;
 }
