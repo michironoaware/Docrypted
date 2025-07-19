e044c52c7446fbe658d9f60ace0a8560ad11cb26
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-18 22:04:43 -0300

    feat: add string related utilities

---
+++ src/shared/StringHelper.ts
@@ -0,0 +1,11 @@
+﻿export namespace StringHelper {
+	const whitespaceChars = " \t\n\r\v\f";
+
+	export function isWhiteSpace(str: string) {
+		if (str.length === 0) return true;
+
+		for (const char of str) if ((c) => !whitespaceChars.includes(c)) return false;
+
+		return true;
+	}
+}
